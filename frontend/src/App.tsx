dca531b004874df57b8e489dd4198ccf681e0532
--NAI-- <118068499+Pakaplong@users.noreply.github.com>
2026-03-30 01:33:21 +0700

    feat: Add Brands and About pages, update custom icons and footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import HomePage from './pages/Home';
 import LoginPage from './pages/Login';
 import ProductsPage from './pages/Products';
 import ComparePage from './pages/Compare';
+import BrandsPage from './pages/Brands';
+import AboutPage from './pages/About';
 // import หน้าอื่นๆ เข้ามาตรงนี้...
 
 function App() {
@@ -18,6 +20,8 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/products" element={<ProductsPage />} />
         <Route path="/compare" element={<ComparePage />} />
+        <Route path="/brands" element={<BrandsPage />} />
+        <Route path="/about" element={<AboutPage />} />
         
       </Routes>
     </BrowserRouter>
